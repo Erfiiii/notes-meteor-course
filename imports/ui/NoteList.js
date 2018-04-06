2bd7e92a633d94a30cf860def8f4c27369ea732e
Erfan Hasanzadeh <er.hasanzadeh@gmail.com>
2018-04-06 13:58:22 +0430

    Mark Selected note in list

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -1,34 +1,43 @@
-import React from 'react';
-import {Meteor} from 'meteor/meteor'
-import {createContainer} from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { createContainer } from "meteor/react-meteor-data";
+import PropTypes from "prop-types";
+import { Session } from "meteor/session";
 
-import {Notes} from '../api/notes'
-import NoteListHeader from './NoteListHeader';
-import NoteListItem from './NoteListItem';
-import NoteListEmptyItem from './NoteListEmptyItem'
+import { Notes } from "../api/notes";
+import NoteListHeader from "./NoteListHeader";
+import NoteListItem from "./NoteListItem";
+import NoteListEmptyItem from "./NoteListEmptyItem";
 
-export const NoteList = (props)=>{
-    return (
-        <div>
-            <NoteListHeader/>
-            NoteList {props.notes.length}
-            {props.notes.length === 0 ? <NoteListEmptyItem/> : undefined}
-            {props.notes.map((note)=>{
-                return <NoteListItem key={note._id} note={note} />
-            })}
-        </div>
-    );
+export const NoteList = props => {
+  return (
+    <div>
+      <NoteListHeader />
+      NoteList {props.notes.length}
+      {props.notes.length === 0 ? <NoteListEmptyItem /> : undefined}
+      {props.notes.map(note => {
+        return <NoteListItem key={note._id} note={note} />;
+      })}
+    </div>
+  );
 };
 
 NoteList.propTypes = {
-    notes:PropTypes.array.isRequired
-}
+  notes: PropTypes.array.isRequired
+};
 
-export default createContainer(()=>{
-    Meteor.subscribe('notes');
+export default createContainer(() => {
+  const selectedNoteId = Session.get("selectedNoteId");
+  Meteor.subscribe("notes");
 
-    return {
-        notes: Notes.find().fetch()
-    }
-},NoteList)
+  return {
+    notes: Notes.find()
+      .fetch()
+      .map(note => {
+        return {
+          ...note,
+          selected: note._id === selectedNoteId
+        };
+      })
+  };
+}, NoteList);
